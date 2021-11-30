0d61573d6071835eb9e13e0548d338c00407c1f5
harinez2 <yonishi@student.42tokyo.jp>
2021-11-30 20:04:09 +0900

    changed var type

--- src/vector.hpp
+++ src/vector.hpp
@@ -8,10 +8,10 @@ namespace ft {
 template <typename T, typename Allocator = std::allocator<T> >
 class vector {
  public:
-  vector() : size_(default_size_), end_(0) {
-    // alc = Allocator();
-    data_ = alc.allocate(default_size_);
-    size_ = default_size_;
+  vector() {
+    first_ = alc.allocate(default_size_);
+    last_ = first_;
+    reserved_last_ = first_ + default_size_;
   }
 
   vector(const vector& rhs) { *this = rhs; }
@@ -23,34 +23,35 @@ class vector {
   }
 
   ~vector() {
-    for (std::size_t i = 0; i != end_; ++i)
-      alc.destroy(data_ + i);
-    alc.deallocate(data_, size_);
+    for (std::size_t i = 0; i != size(); ++i)
+      alc.destroy(first_ + i);
+    alc.deallocate(first_, reserved_last_ - first_);
   }
 
   void push_back(const T& var) {
-    if (end_ == size_) {
+    if (last_ == reserved_last_) {
       //reallocate
     }
-    alc.construct(data_ + end_++, var);
+    alc.construct(first_ + size(), var);
+    ++last_;
   }
 
   T& operator[](std::size_t idx) const {
-    if (idx < 0 || size_ <= idx)
+    if (idx < 0 || size() <= idx)
       throw std::exception();
-    return data_[idx];
+    return first_[idx];
   }
 
-  // typename T::iterator begin() { return data_; }
-  // typename T::iterator end() { return data_ + end_; }
-  std::size_t size() { return end_; }
+  // typename T::iterator begin() { return first_; }
+  // typename T::iterator end() { return first_ + last_; }
+  std::size_t size() const { return last_ - first_; }
 
  private:
   static const int default_size_ = 8;
   Allocator alc;
-  std::size_t size_;
-  std::size_t end_;
-  T* data_;
+  T* first_;
+  T* last_;
+  T* reserved_last_;
 };
 
 } // namespace ft
