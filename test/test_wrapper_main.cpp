cd16f64291da144a56fa909f607ed20d07818348
harinez2 <yonishi@student.42tokyo.jp>
2021-12-26 23:29:08 +0900

    refactoring test wrapper

--- test/test_wrapper_main.cpp
+++ test/test_wrapper_main.cpp
@@ -1,5 +1,4 @@
 
-//#ifdef MANUAL_TEST
 #include <cstddef>
 #include <cassert>
 #include <iostream>
@@ -9,25 +8,15 @@
 #define EXPECT_FALSE(x) assert(x == false)
 #define EXPECT_EQ(x,y) assert(x == y)
 #define TEST_F(x,y) void x##_##y(VectorTest& vt)
-//#endif
-
-#include "test_vector.hpp"
-#include "../src/vector.hpp"
 
 namespace testing {
 class Test {};
 }
 #include "test_vector.cpp"
 
-
-
-
-//#ifdef MANUAL_TEST
 int main() {
   VectorTest vt;
 
-  // TEST_F(vector, test_VectorTest_constructor);
   VectorTest_int_constructor_copy(vt);
   return 0;
 }
-//#endif
