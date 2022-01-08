52991de7641c51ace076c187fc22327b5a010d1d
harinez2 <yonishi@student.42tokyo.jp>
2022-01-08 23:29:41 +0900

    added test_map

---
+++ test/test_map.cpp
@@ -0,0 +1,38 @@
+#include "../src/map.hpp"
+// #include "test_vector.hpp"
+
+#include <map>
+
+class MapTest : public ::testing::Test {
+ public:
+  MapTest() {
+    // vft_.push_back(42);
+    // vft_.push_back(1);
+    // vft_.push_back(0);
+    // vft_.push_back(-42);
+    // vft_.push_back(4242);
+    
+    // vstd_.push_back(42);
+    // vstd_.push_back(1);
+    // vstd_.push_back(0);
+    // vstd_.push_back(-42);
+    // vstd_.push_back(4242);
+  }
+  ~MapTest() {
+  }
+
+ protected:
+//   ft::map<int> mft_;
+//   std::map<int> mstd_;
+};
+
+//------------------------------------------------------
+// constructor (int)
+
+TEST_F(MapTest, int_constructor_empty) {
+//   ft::vector<int> vft;
+//   std::vector<int> vstd;
+//   EXPECT_EQ(vft.size(), 0);
+//   EXPECT_TRUE(vft.empty());
+//   compare_with_std_vector(vft, vstd);
+}
