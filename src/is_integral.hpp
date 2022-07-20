076fe2a522d89fd243ed3ef9ced1263f5f559a2c
harinez2 <yonishi@student.42tokyo.jp>
2022-07-20 21:31:42 +0900

    removed compile error ( added remove_cv )

--- src/is_integral.hpp
+++ src/is_integral.hpp
@@ -4,12 +4,12 @@
 namespace ft {
 
 // integral_constant
-template <class T, T val>
+template <typename T, T val>
 struct integral_constant
 {
   typedef T                           value_type;
   typedef integral_constant<T, val>   type;
-  static const T value = val;
+  enum { value = val };
 
   operator T() const { return val; }
 };
@@ -18,18 +18,48 @@ struct integral_constant
 typedef integral_constant<bool, true> true_type;
 typedef integral_constant<bool, false> false_type;
 
+// remove_const
+template<typename T>
+struct remove_const {
+  typedef T     type;
+};
+template<typename T>
+struct remove_const<T const> {
+  typedef T     type;
+};
+
+// remove_volatile
+template<typename T>
+struct remove_volatile {
+  typedef T     type;
+};
+template<typename T>
+struct remove_volatile<T volatile> {
+  typedef T     type;
+};
+
+// remove_cv
+template <class T>
+struct remove_cv {
+  typedef typename remove_const<typename remove_volatile<T>::type>::type  type;
+};
+
+// is_integral_helper
+template<class T> struct is_integral_helper : public false_type {};
+template<> struct is_integral_helper<unsigned char> : public true_type {};
+template<> struct is_integral_helper<unsigned short> : public true_type{};
+template<> struct is_integral_helper<unsigned int> : public true_type{};
+template<> struct is_integral_helper<unsigned long> : public true_type{};
+template<> struct is_integral_helper<signed char> : public true_type{};
+template<> struct is_integral_helper<short> : public true_type{};
+template<> struct is_integral_helper<int> : public true_type{};
+template<> struct is_integral_helper<long> : public true_type{};
+template<> struct is_integral_helper<char> : public true_type{};
+template<> struct is_integral_helper<bool> : public true_type{};
+
 // is_integral
-template<class T> struct is_integral : public false_type {};
-template<> struct is_integral<unsigned char> : public true_type {};
-template<> struct is_integral<unsigned short> : public true_type{};
-template<> struct is_integral<unsigned int> : public true_type{};
-template<> struct is_integral<unsigned long> : public true_type{};
-template<> struct is_integral<signed char> : public true_type{};
-template<> struct is_integral<short> : public true_type{};
-template<> struct is_integral<int> : public true_type{};
-template<> struct is_integral<long> : public true_type{};
-template<> struct is_integral<char> : public true_type{};
-template<> struct is_integral<bool> : public true_type{};
+template<typename T>
+struct is_integral : public is_integral_helper<typename remove_cv<T>::type>::type { };
 
 } // namespace ft
 
