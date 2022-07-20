5921009a33634c6fa3f6c31a98e50be553ac42ad
harinez2 <yonishi@student.42tokyo.jp>
2022-07-20 21:51:02 +0900

    fixed is_integral test cases

--- test/test_is_integral.cpp
+++ test/test_is_integral.cpp
@@ -1,8 +1,5 @@
 #include "../src/is_integral.hpp"
 
-#include <boost/type_traits/is_integral.hpp>
-#include <boost/core/is_same.hpp>
-
 class IsIntegralTest : public ::testing::Test {
  public:
   IsIntegralTest() {
@@ -19,50 +16,50 @@ class IsIntegralTest : public ::testing::Test {
 TEST_F(IsIntegralTest, basic_cases) {
   bool ret;
 
-  EXPECT_TRUE(boost::is_integral<int>::value);
-  ret = boost::core::is_same<boost::is_integral<int>::value_type, int>::value;
+  EXPECT_TRUE(std::is_integral<int>::value);
+  ret = std::is_same<std::is_integral<int>::value_type, int>::value;
+  EXPECT_FALSE(ret);
+  ret = std::is_same<std::is_integral<int>::value_type, bool>::value;
   EXPECT_TRUE(ret);
-  ret = boost::core::is_same<boost::is_integral<int>::value_type, bool>::value;
-  EXPECT_FALSE(ret);
-  ret = boost::core::is_same<boost::is_integral<int>::type, boost::true_type>::value;
-  EXPECT_FALSE(ret);
-  EXPECT_TRUE(boost::is_integral<int>());
+  ret = std::is_same<std::is_integral<int>::type, std::true_type>::value;
+  EXPECT_TRUE(ret);
+  EXPECT_TRUE(std::is_integral<int>());
 
   EXPECT_TRUE(ft::is_integral<int>::value);
-  ret = boost::core::is_same<ft::is_integral<int>::value_type, int>::value;
+  ret = std::is_same<ft::is_integral<int>::value_type, int>::value;
+  EXPECT_FALSE(ret);
+  ret = std::is_same<ft::is_integral<int>::value_type, bool>::value;
   EXPECT_TRUE(ret);
-  ret = boost::core::is_same<ft::is_integral<int>::value_type, bool>::value;
-  EXPECT_FALSE(ret);
-  ret = boost::core::is_same<ft::is_integral<int>::type, ft::true_type>::value;
-  EXPECT_FALSE(ret);
+  ret = std::is_same<ft::is_integral<int>::type, ft::true_type>::value;
+  EXPECT_TRUE(ret);
   EXPECT_TRUE(ft::is_integral<int>());
 }
 
 TEST_F(IsIntegralTest, pointer_cases) {
   bool ret;
 
-  EXPECT_FALSE(boost::is_integral<int*>::value);
-  ret = boost::core::is_same<boost::is_integral<int*>::value_type, bool>::value;
-  EXPECT_FALSE(ret);
-  ret = boost::core::is_same<boost::is_integral<int*>::type, boost::false_type>::value;
-  EXPECT_FALSE(ret);
-  EXPECT_FALSE(boost::is_integral<int*>());
+  EXPECT_FALSE(std::is_integral<int*>::value);
+  ret = std::is_same<std::is_integral<int*>::value_type, bool>::value;
+  EXPECT_TRUE(ret);
+  ret = std::is_same<std::is_integral<int*>::type, std::false_type>::value;
+  EXPECT_TRUE(ret);
+  EXPECT_FALSE(std::is_integral<int*>());
 
   EXPECT_FALSE(ft::is_integral<int*>::value);
-  ret = boost::core::is_same<ft::is_integral<int*>::value_type, bool>::value;
-  EXPECT_FALSE(ret);
-  ret = boost::core::is_same<ft::is_integral<int*>::type, ft::false_type>::value;
-  EXPECT_FALSE(ret);
+  ret = std::is_same<ft::is_integral<int*>::value_type, bool>::value;
+  EXPECT_TRUE(ret);
+  ret = std::is_same<ft::is_integral<int*>::type, ft::false_type>::value;
+  EXPECT_TRUE(ret);
   EXPECT_FALSE(ft::is_integral<int*>());
 }
 
 TEST_F(IsIntegralTest, integral_cases) {
-  EXPECT_TRUE(boost::is_integral<bool>::value);
-  EXPECT_TRUE(boost::is_integral<char>::value);
-  EXPECT_TRUE(boost::is_integral<unsigned char>::value);
-  EXPECT_TRUE(boost::is_integral<unsigned short>::value);
-  EXPECT_TRUE(boost::is_integral<unsigned int>::value);
-  EXPECT_TRUE(boost::is_integral<unsigned long>::value);
+  EXPECT_TRUE(std::is_integral<bool>::value);
+  EXPECT_TRUE(std::is_integral<char>::value);
+  EXPECT_TRUE(std::is_integral<unsigned char>::value);
+  EXPECT_TRUE(std::is_integral<unsigned short>::value);
+  EXPECT_TRUE(std::is_integral<unsigned int>::value);
+  EXPECT_TRUE(std::is_integral<unsigned long>::value);
 
   EXPECT_TRUE(ft::is_integral<bool>::value);
   EXPECT_TRUE(ft::is_integral<char>::value);
@@ -73,11 +70,11 @@ TEST_F(IsIntegralTest, integral_cases) {
 }
 
 TEST_F(IsIntegralTest, non_integral_cases) {
-  EXPECT_FALSE(boost::is_integral<my_enum>::value);
-  EXPECT_FALSE(boost::is_integral<int&>::value);
-  EXPECT_FALSE(boost::is_integral<int[1]>::value);
-  EXPECT_FALSE(boost::is_integral<int ()>::value);
-  EXPECT_FALSE(boost::is_integral<float>::value);
+  EXPECT_FALSE(std::is_integral<my_enum>::value);
+  EXPECT_FALSE(std::is_integral<int&>::value);
+  EXPECT_FALSE(std::is_integral<int[1]>::value);
+  EXPECT_FALSE(std::is_integral<int ()>::value);
+  EXPECT_FALSE(std::is_integral<float>::value);
   
   EXPECT_FALSE(ft::is_integral<my_enum>::value);
   EXPECT_FALSE(ft::is_integral<int&>::value);
