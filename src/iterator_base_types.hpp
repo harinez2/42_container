41e87a33ed5dba0a0bad5c7e143ca5e5a98483dd
harinez2 <yonishi@student.42tokyo.jp>
2022-07-21 23:05:13 +0900

    iterator base types bug fix

--- src/iterator_base_types.hpp
+++ src/iterator_base_types.hpp
@@ -2,14 +2,9 @@
 #define ITERATOR_BASE_TYPES_HPP
 
 #include <cstddef>
+#include <iterator>
 
 namespace ft {
-
-// struct input_iterator_tag {};
-// struct output_iterator_tag {};
-// struct forward_iterator_tag : public input_iterator_tag {};
-// struct bidirectional_iterator_tag : public forward_iterator_tag {};
-// struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 
 template <class Iterator>
 struct iterator_traits {
@@ -22,20 +17,20 @@ struct iterator_traits {
 
 template <class T>
 struct iterator_traits<T*> {
-  typedef ptrdiff_t                     difference_type;
-  typedef T                             value_type;
-  typedef T*                            pointer;
-  typedef T&                            reference;
-  typedef std::random_access_iterator_tag    iterator_category;
+  typedef std::ptrdiff_t                   difference_type;
+  typedef T                                value_type;
+  typedef T*                               pointer;
+  typedef T&                               reference;
+  typedef std::random_access_iterator_tag  iterator_category;
 };
 
 template <class T>
 struct iterator_traits<const T*> {
-  typedef ptrdiff_t                     difference_type;
-  typedef T                             value_type;
-  typedef const T*                      pointer;
-  typedef const T&                      reference;
-  typedef std::random_access_iterator_tag    iterator_category;
+  typedef std::ptrdiff_t                   difference_type;
+  typedef T                                value_type;
+  typedef const T*                         pointer;
+  typedef const T&                         reference;
+  typedef std::random_access_iterator_tag  iterator_category;
 };
 
 } // namespace ft
