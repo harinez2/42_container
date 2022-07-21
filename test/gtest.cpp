0d4ba9ab1010c77856750b485a345670d9cdcf97
harinez2 <yonishi@student.42tokyo.jp>
2022-07-21 22:51:21 +0900

    added equal and the tests

--- test/gtest.cpp
+++ test/gtest.cpp
@@ -3,7 +3,10 @@
 // #define BIG_TEST_SIZE 10000
 #define BIG_TEST_SIZE 10
 
-#include "test_is_integral.cpp"
-#include "test_enable_if.cpp"
-#include "test_utility.cpp"
+// OK
+// #include "test_is_integral.cpp"
+// #include "test_enable_if.cpp"
+// #include "test_utility.cpp"
+
+#include "test_equal.cpp"
 // #include "test_vector.cpp"
---
+++ test/test_equal.cpp
@@ -0,0 +1,52 @@
+#include "../src/algorithm.hpp"
+
+#include <algorithm>
+#include <vector>
+
+class EqualTest : public ::testing::Test {
+ public:
+  EqualTest() {
+  }
+  ~EqualTest() {
+  }
+};
+
+//------------------------------------------------------
+
+TEST_F(EqualTest, basic_cases) {
+  std::vector<int> std_v1 = {4, 2, 0};
+  std::vector<int> std_v2 = {4, 2, 0};
+  std::vector<int> std_v3 = {4, 2, 1};
+  std::vector<int> std_v4 = {4, 2, 0, 0};
+
+  EXPECT_TRUE(std::equal(std::begin(std_v1), std::end(std_v1), std::begin(std_v2)));
+  EXPECT_TRUE(ft::equal(std::begin(std_v1), std::end(std_v1), std::begin(std_v2)));
+  
+  EXPECT_FALSE(std::equal(std::begin(std_v1), std::end(std_v1), std::begin(std_v3)));
+  EXPECT_FALSE(ft::equal(std::begin(std_v1), std::end(std_v1), std::begin(std_v3)));
+  
+  EXPECT_TRUE(std::equal(std::begin(std_v1), std::end(std_v1), std::begin(std_v4)));
+  EXPECT_TRUE(ft::equal(std::begin(std_v1), std::end(std_v1), std::begin(std_v4)));
+}
+
+bool predicator_double(int x, int y) {
+  if (x * 2 == y)
+    return true;
+  return false;
+}
+
+TEST_F(EqualTest, with_pred) {
+  std::vector<int> std_v1 = {4, 2, 0};
+  std::vector<int> std_v2 = {8, 4, 0};
+  std::vector<int> std_v3 = {8, 4, 1};
+  std::vector<int> std_v4 = {8, 4, 0, 0};
+
+  EXPECT_TRUE(std::equal(std::begin(std_v1), std::end(std_v1), std::begin(std_v2), predicator_double));
+  EXPECT_TRUE(ft::equal(std::begin(std_v1), std::end(std_v1), std::begin(std_v2), predicator_double));
+  
+  EXPECT_FALSE(std::equal(std::begin(std_v1), std::end(std_v1), std::begin(std_v3), predicator_double));
+  EXPECT_FALSE(ft::equal(std::begin(std_v1), std::end(std_v1), std::begin(std_v3), predicator_double));
+  
+  EXPECT_TRUE(std::equal(std::begin(std_v1), std::end(std_v1), std::begin(std_v4), predicator_double));
+  EXPECT_TRUE(ft::equal(std::begin(std_v1), std::end(std_v1), std::begin(std_v4), predicator_double));
+}
